3e1da0fa65b026eadabf50189dfe3d0f49d97804
Joe Norman <nojosep@amazon.com>
2019-11-20 12:36:59 -0800

    minor debug of writeToS3

--- lambdas/writeToS3/writeToS3.js
+++ lambdas/writeToS3/writeToS3.js
@@ -4,7 +4,7 @@ var s3 = new AWS.S3();
 exports.handler = async(event, context) => {
     var csvdata = "id,timestamp,keyword,sentiment" + String.fromCharCode(13);
     for(var i = 0; i < event.topics.length; i++) {
-        csvdata += (event.id + "," + event.created_at + "," + event.topics[i] + "," + event.sentiment + String.fromCharCode(13));
+        csvdata += (event.id + "," + event.created_at + "," + event.topics[i] + "," + event.sentiment.Sentiment + String.fromCharCode(13));
     }
     var params = {
         Bucket: process.env.S3BucketForAnalytics,
